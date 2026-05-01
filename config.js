3d61c209d705324b4f3314dd3411682899ed37f8
webboost-com-pl <kontakt.webboost@gmail.com>
2026-05-01 22:04:50 +0200

    Create config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+const supabaseUrl = "https://sdpxqugrvssnkesjfkhy.supabase.co";
+
+const supabaseClient = supabase.createClient(
+  supabaseUrl,
+  "sb_publishable_FoFzvVpIn00A6wjsXlxHdw_K2hDZpYh"
+);
